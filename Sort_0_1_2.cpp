d421967e90af397fa4e41250033394530a06d998
Maulik Ranadive <80736633+Maulik176@users.noreply.github.com>
2023-06-05 15:43:59 +0530

    Create Sort_0_1_2.cpp

---
+++ Sort_0_1_2.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h> 
+void sort012(int *arr, int n)
+{
+   sort(arr,arr+n);
+   //   Write your code here
+}
+//My solution Link :- https://www.codingninjas.com/codestudio/problems/sort-0-1-2_8230695?challengeSlug=striver-sde-challenge&leftPanelTab=1
