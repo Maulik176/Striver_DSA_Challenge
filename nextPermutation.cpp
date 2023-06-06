ee16a3a8c83185887cf182ce7086713e135b709a
Maulik Ranadive <80736633+Maulik176@users.noreply.github.com>
2023-06-06 14:01:38 +0530

    Create nextPermutation.cpp

---
+++ nextPermutation.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h> 
+vector<int> nextPermutation(vector<int> &permutation, int n)
+{
+    next_permutation(permutation.begin(),permutation.end());
+    return permutation;
+    //  Write your code here.
+}
+
+//My Solution link :- https://www.codingninjas.com/codestudio/problems/next-permutation_8230741?challengeSlug=striver-sde-challenge&leftPanelTab=1
